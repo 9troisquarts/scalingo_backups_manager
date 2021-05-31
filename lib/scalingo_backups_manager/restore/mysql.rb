f39e70972d7955ee2e872f1ea9422469cf732b2d
Kevin Clercin <kevin@9troisquarts.com>
2021-05-31 16:49:29 +0200

    feat: Restore mysql dump on local

--- lib/scalingo_backups_manager/restore/mysql.rb
+++ lib/scalingo_backups_manager/restore/mysql.rb
@@ -17,12 +17,15 @@ module ScalingoBackupsManager
         destination_path = filename.split("/")
         backup_name = destination_path.pop.gsub(".tar.gz", "")
         destination_path = destination_path.join("/") + backup_name + "/"
-        p destination_path
         if Dir.exist?(destination_path)
           puts "Unzipped backup is already present, skipping..."
         else
-          Dir.mkdir(destination_path) unless Dir.exist?(destination_path)
+          unless Dir.exist?(destination_path)
+            p "Creating destination folder..."
+            Dir.mkdir(destination_path)
+          end
           untar_cmd = "tar zxvf #{filename} -C #{destination_path}"
+          p "Untar file with command #{untar_cmd}"
           system untar_cmd
         end
 
@@ -31,10 +34,10 @@ module ScalingoBackupsManager
           host: rails_db_config["host"],
           database: rails_db_config["database"],
           password: rails_db_config["password"],
-          user: rails_db_config["user"],
+          user: rails_db_config["username"],
         }
 
-        restore_cmd = "/usr/bin/env mysql #{config[:database]} -h #{opts[:host] || config[:host]}"
+        restore_cmd = "/usr/bin/env mysql -h #{opts[:host] || config[:host]}"
 
         if config[:user].present?
           restore_cmd << " -u #{config[:user]}"
@@ -44,15 +47,23 @@ module ScalingoBackupsManager
           end
         end
 
-        if config[:port].present?
+        if config[:port].present? || opts[:port].present?
           restore_cmd << " -P #{opts[:port] || config[:port] || 5432}"
         end
 
-        restore_cmd << " < #{destination_path}"
+        restore_cmd << " #{config[:database]}"
+
+        file_path = Dir["#{destination_path}*.sql"]
+        if file_path.empty?
+          puts "*** No SQL file found in tar ***"
+          return
+        end
+        restore_cmd << " < #{file_path.first}"
 
         puts "*** Restoring backup to mysql database ***"
+        puts "Command: #{restore_cmd}"
         system(restore_cmd)
-        FileUtils.rm_r destination_path unless opts[:skip_rm]
+        #FileUtils.rm_r destination_path unless opts[:skip_rm]
       end
 
     end
